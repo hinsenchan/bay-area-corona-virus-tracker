72d5cbcb8ed95eddb441eca4c47418478a8eb2c9
Hinsen Chan <hinsenchan@gmail.com>
2020-03-22 21:22:55 -0700

    Implement logic to cache fetch data to local storage.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {isEmpty} from "lodash";
+import moment from "moment";
+import store from "store2";
 import logo from './logo.svg';
 import './App.css';
 
-async function fetchData() {
+async function fetchAPIData() {
   const response = await fetch("https://files.sfchronicle.com/project-feeds/covid19_us_cases_ca_by_county_.json");
   const payload = await response.json();
-  console.log(payload);
+  return payload;
+}
+
+async function fetchAndStore(key, data, setData) {
+  if (isEmpty(data)) {
+    const payload = await fetchAPIData();
+    setStoreData(key, payload);
+    setData(payload);
+  }
+}
+
+function setStoreData(key, payload) {
+  store.set(key, payload);
+}
+
+function getStoreData(key) {
+  return store.get(key);
 }
 
 function App() {
-  fetchData()
+  const key = moment().format("YYYY-MM-DD");
+  const storeData = getStoreData(key) || {};
+  const [data, setData] = useState(storeData);
+
+  useEffect(() => {
+    fetchAndStore(key, data, setData);
+  }, [key, data]);
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
