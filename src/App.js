7c70eefd206f09537aeb485f719bc735d6a2ef08
Hinsen Chan <hinsenchan@gmail.com>
2020-03-22 23:40:10 -0700

    Set project id for GA.

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import './App.css';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCGQ0tYppWFJkuSxBhOpkH0xVDmX245Vdc",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
+  authDomain: "bay-area-corona-virus-tracker.firebaseapp.com",
+  databaseURL: "https://bay-area-corona-virus-tracker.firebaseio.com",
+  projectId: "bay-area-corona-virus-tracker",
+  storageBucket: "bay-area-corona-virus-tracker.appspot.com",
   messagingSenderId: "637908496727",
   appId: "2:637908496727:web:a4284b4c99e329d5",
   measurementId: "G-9VP01NDSXJ"
