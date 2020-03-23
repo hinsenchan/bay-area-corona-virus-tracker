80d618951e42326d71717476b37f703447298847
Hinsen Chan <hinsenchan@gmail.com>
2020-03-22 23:34:22 -0700

    Init GA via firebase.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React, {useState, useEffect} from 'react';
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import {isEmpty, isEqual, keyBy, map, mapValues, omit, sortBy, toNumber} from "lodash";
+import * as firebase from "firebase/app";
+import "firebase/analytics";
 import moment from "moment";
 import store from "store2";
 import './App.css';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGQ0tYppWFJkuSxBhOpkH0xVDmX245Vdc",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "637908496727",
+  appId: "2:637908496727:web:a4284b4c99e329d5",
+  measurementId: "G-9VP01NDSXJ"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 async function fetchAPIData() {
   const response = await fetch("https://files.sfchronicle.com/project-feeds/covid19_us_cases_ca_by_county_.json");
