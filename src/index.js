be16fd5df64b05957c5160f511e2ed60840cfb81
Hinsen Chan <hinsenchan@gmail.com>
2020-03-27 23:20:31 -0700

    Use white background for app.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "typeface-roboto";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: "#ffffff"
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
