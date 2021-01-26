20ef2de64125b386cd28edf07409201b63d48eb4
Hinsen Chan <hinsenchan@gmail.com>
2021-01-25 23:05:28 -0800

    Extract Footer to its own file.

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components/macro";
 import DrawerButtonGroup from "./components/DrawerButtonGroup";
+import Footer from "./components/Footer";
 import PopperButtonGroup from "./components/PopperButtonGroup";
 import { useFetchAndStore } from "./hooks/useFetchAndStore";
 import { NOTABLE_EVENTS } from "./utils/eventConstants";
@@ -389,41 +390,7 @@ function App() {
           handleGranularity={handleGranularity}
         />
       </StyledFabContainer>
-
-      <Typography align="center" variant="caption" component="h6">
-        <p>
-          <span>
-            The time series data on this page is sourced from SF
-            Chronicle's&nbsp;
-          </span>
-          <a href="https://projects.sfchronicle.com/2020/coronavirus-map/">
-            Coronavirus Tracker
-          </a>
-          <span>
-            . Original data compiled by the SF Chronicle was collected from
-            the&nbsp;
-            <a href="https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html">
-              Centers for Disese Control
-            </a>
-            ,&nbsp;
-            <a href="https://www.cdph.ca.gov/Programs/CID/DCDC/Pages/Immunization/nCOV2019.aspx">
-              California Department of Public Health
-            </a>
-            , and individual county public health departments.
-          </span>
-        </p>
-        <p>
-          <span>Created by Hinsen Chan</span>
-          <br />
-          <a
-            href="https://github.com/hinsenchan"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            https://github.com/hinsenchan
-          </a>
-        </p>
-      </Typography>
+      <Footer />
     </StyledContainer>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+function Footer() {
+  return (
+    <Typography align="center" variant="caption" component="h6">
+      <p>
+        <span>
+          The time series data on this page is sourced from SF Chronicle's&nbsp;
+        </span>
+        <a href="https://projects.sfchronicle.com/2020/coronavirus-map/">
+          Coronavirus Tracker
+        </a>
+        <span>
+          . Original data compiled by the SF Chronicle was collected from
+          the&nbsp;
+          <a href="https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html">
+            Centers for Disese Control
+          </a>
+          ,&nbsp;
+          <a href="https://www.cdph.ca.gov/Programs/CID/DCDC/Pages/Immunization/nCOV2019.aspx">
+            California Department of Public Health
+          </a>
+          , and individual county public health departments.
+        </span>
+      </p>
+      <p>
+        <span>Created by Hinsen Chan</span>
+        <br />
+        <a
+          href="https://github.com/hinsenchan"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://github.com/hinsenchan
+        </a>
+      </p>
+    </Typography>
+  );
+}
+
+export default Footer;
