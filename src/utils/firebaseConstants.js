f23d71792418bca424b83d053e2187f71bb8e247
Hinsen Chan <hinsenchan@gmail.com>
2021-01-31 21:56:06 -0800

    Add reminder that firebase API keys are safe to check in.

--- src/utils/firebaseConstants.js
+++ src/utils/firebaseConstants.js
@@ -1,3 +1,10 @@
+/**
+ * API keys for Firebase services are ok to include in code or checked-in
+ * config files. Unlike how API keys are typically used, API keys for Firebase
+ * services are not used to control access to backend resources.
+ * https://stackoverflow.com/questions/35418143/how-to-restrict-firebase-data-modification
+ * https://firebase.google.com/docs/projects/api-keys
+ */
 export const FIREBASE_CONFIG = {
   apiKey: "AIzaSyAOs83YSuTuIKk-Zob_QFw49IM6_k-AM3w",
   authDomain: "bay-area-corona-virus-tracker.firebaseapp.com",
